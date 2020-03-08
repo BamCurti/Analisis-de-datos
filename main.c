e84e16ed96e899beef1b534da2778fc556db08cf
Edgar Vallejo <bam_vllj98@outlook.com>
2020-03-08 15:49:22 -0600

    Primer commit

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: 8 mar. 2020
+ *      Author: Edgar Vallejo Curti
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	setbuf(stdout, NULL);
+	return 0;
+}
+

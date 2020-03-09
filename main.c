071755e7f1902f0e73013c8782ac42f1435df266
Edgar Vallejo Curti <bam_vllj98@outlook.com>
2020-03-08 19:04:38 -0600

    Funciones de lectura de archivos creada, falta limpiar el dato, y tiene basura

--- main.c
+++ main.c
@@ -7,11 +7,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *leerArchivo();
+void reallocString(char **, int*);
+void readLine(char **, int*);
+
 int main(){
 	setbuf(stdout, NULL);
+	char *informacion = leerArchivo();
 
+	printf("caracteres después: %d\n\r", )
 
+	printf("%s", informacion);
 
 	return 0;
 }
 
+char * leerArchivo(){
+	char *direccion = NULL;
+	int largo = 0;
+
+	printf("Ingrese direccion de documento:\n");
+	reallocString(&direccion, &largo);
+	readLine(&direccion, &largo);
+
+	FILE *archivo = fopen((char*)direccion, "r");
+
+    fseek(archivo, 0, SEEK_END);    /* file pointer at the end of file */
+	long nbytes = ftell(archivo);
+	rewind(archivo);
+	char *contenido = (char *)malloc(nbytes*sizeof(char));
+	int  i = 0;
+
+	while(i < nbytes){
+		contenido[i] = fgetc(archivo);
+		i++;
+	}
+
+	fclose(archivo);
+	return contenido;
+}
+void reallocString(char **lst, int *largo){
+    (*largo) += 1;
+    char *temp = (char*) realloc(*lst, (*largo) * sizeof (char));
+    *lst = temp;
+}
+void readLine(char **lst, int *largo){
+    int c;
+    int pos = 0;
+    fflush(stdin);
+
+    c = getchar();
+    while (c != '\n' && c != EOF) {
+		reallocString(lst, largo);
+        (*lst)[pos] =(char) c;
+        pos++;
+        c = getchar();
+    }
+    reallocString(lst, largo);
+    (*lst)[pos] = '\0';
+}
